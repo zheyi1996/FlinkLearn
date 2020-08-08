faf8298be037b954b21ee131cc2a6b7da8930bae
WeiLiuGuo <305616809@qq.com>
2020-08-08 11:13:58 +0800

    2020-8-8

---
+++ src/main/Scala/cn/neu/edu/wlg/flinkConnector/KafkaConnectorDemo.scala
@@ -0,0 +1,16 @@
+package cn.neu.edu.wlg.flinkConnector
+
+import java.util.Properties
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+object KafkaConnectorDemo {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment;
+    val properties = new Properties()
+//    properties.setProperty()
+//    properties.setProperty()
+//    properties.setProperty()
+    //https://juejin.im/post/6844903946297802760
+  }
+}
