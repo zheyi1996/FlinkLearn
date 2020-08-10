815d87ca90d45b846987082d2db2d07f62b1273d
WeiLiuGuo <305616809@qq.com>
2020-08-10 21:20:55 +0800

    2020-8-10

--- src/main/Scala/cn/neu/edu/wlg/flinkConnector/KafkaConnectorDemo.scala
+++ src/main/Scala/cn/neu/edu/wlg/flinkConnector/KafkaConnectorDemo.scala
@@ -2,15 +2,22 @@ package cn.neu.edu.wlg.flinkConnector
 
 import java.util.Properties
 
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+//import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
+import org.apache.flink.streaming.api.scala._
 
 object KafkaConnectorDemo {
   def main(args: Array[String]): Unit = {
-    val env = StreamExecutionEnvironment.getExecutionEnvironment;
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
     val properties = new Properties()
-//    properties.setProperty()
-//    properties.setProperty()
-//    properties.setProperty()
-    //https://juejin.im/post/6844903946297802760
+    properties.setProperty("bootstrap.servers", "master:9092")
+    properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    println("----------test---------")
+    val stream = env.addSource(new FlinkKafkaConsumer[String]("test", new SimpleStringSchema, properties))
+    stream.print("stream")
+    env.execute("KafkaTest")
   }
 }
